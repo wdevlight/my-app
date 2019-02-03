9b8ea93d6f3d5cacda38ffb123d6838684a3462b
devlight <wdevlight@gmail.com>
2019-02-03 17:03:03 +0900

    Updates

---
+++ precache-manifest.2c8d42da07943576dc6b8061ce1f719e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "ca960101f773515e798a",
+    "url": "/my-app/static/js/main.ca960101.chunk.js"
+  },
+  {
+    "revision": "0e493f63e1ac10c82d37",
+    "url": "/my-app/static/js/1.0e493f63.chunk.js"
+  },
+  {
+    "revision": "ca960101f773515e798a",
+    "url": "/my-app/static/css/main.1d38acc0.chunk.css"
+  },
+  {
+    "revision": "cd34c9bbf0f1b836ed652fe46cbfb77b",
+    "url": "/my-app/index.html"
+  }
+];
